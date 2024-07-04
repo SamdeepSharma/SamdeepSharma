05495e9da09eedea198db6314b627a8776a24652
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-07-04 23:59:41 +0530

    added navbar and about non-functional sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import Hero from "@/components/Hero"; 4
 import { TracingBeam } from "@/components/ui/tracing-beam";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <div className="min-h-screen antialiased relative overflow-hidden">
         <Hero />
+        <About/>
       {/* <TracingBeam className="z-10">
       </TracingBeam> */}
     </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "./ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+export default function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Work",
+      link: "/work",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+    {
+      name: "Skills",
+      link: "/skills",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  return (
+    <div className="relative  w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
