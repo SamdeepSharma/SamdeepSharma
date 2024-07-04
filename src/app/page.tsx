ef315c9bd2ae72faf26bdf4e956c010916ceaec0
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-07-04 23:02:10 +0530

    made hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@
 import Image from "next/image";
 import React from "react";
 import { WavyBackground } from "@/components/ui/wavy-background";
-import Hero from "@/components/Hero";
+import Hero from "@/components/Hero"; 4
+import { TracingBeam } from "@/components/ui/tracing-beam";
+
 export default function Home() {
   return (
-    <div className="min-h-screen antialiased relative h-screen overflow-hidden">
-      <Hero />
+    <div className="min-h-screen antialiased relative overflow-hidden">
+        <Hero />
+      {/* <TracingBeam className="z-10">
+      </TracingBeam> */}
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,61 @@
+'use client'
 import React from 'react'
 import { Vortex } from "./ui/vortex";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+import { FlipWords } from './ui/flip-words';
+import { useState, useEffect } from 'react';
+
+const words = [
+  {
+    text: "Welcome",
+  },
+  {
+    text: "to",
+  },
+  {
+    text: "Samdeep",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+  {
+    text: "Sharma's",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+  {
+    text: "Portfolio.",
+  },
+];
 
 const Hero = () => {
+  const flipwords = ["UI/UX Designer", "Software Engineer", "Web Developer", "Pre-final Year Student"];
+
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(true);
+    }, 3700);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-     <div className="w-full mx-auto rounded-md h-screen overflow-hidden">
-     <Vortex
-       backgroundColor="black"
-       rangeY={800}
-       particleCount={500}
-       baseHue={120}
-       className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
-     >
-       <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-         The hell is this?
-       </h2>
-       <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-         This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-         burned and you&apos;ll have a scar.
-       </p>
-       <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-         <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-           Order now
-         </button>
-         <button className="px-4 py-2  text-white ">Watch trailer</button>
-       </div>
-     </Vortex>
-   </div>
+    <div className="w-full mx-auto rounded-md h-screen overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={500}
+        baseHue={120}
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+      >
+        <TypewriterEffectSmooth words={words} />
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        </div>
+        <div className={`text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 ${showText?'':'hidden'}`}>
+        I&apos;m a 
+        <FlipWords words={flipwords} /> <br />
+      </div>
+      </Vortex>
+    </div>
   )
 }
 
